d8d17ac7d21e0575fa82211339b709106544d694
monsieur <alex.carmody@hotmail.com>
2021-10-25 17:13:55 -0700

    intern html

--- generate_site.js
+++ generate_site.js
@@ -34,6 +34,25 @@ const generateTeam = (team) => {
     html.push(engineerHtml);
   }
   const generateIntern = intern => {
-    
+    console.log(intern);
+    let internHtml = `
+      <div class="card" syle="width: 18rem;">
+        <div class="card-header">
+        ${intern.name}<br/>
+      <i class ="fas fa-graduate"></i>Intern</div>
+      <ul class="list-group list-group-flush">
+      <li class="list-group-item">ID: ${intern.id}</li>
+      <li class="list-group-item">Email: <span id="email"><a href="mailto:${intern.email}">${intern.email}</a></span></li>
+      <li class="list-group-item">School: ${intern.school}</li> 
+      </ul>
+      </div>
+      `;
+    html.push(internHtml);
+  };
+
+  for (let i = 0; i < team.lenth; i++) {
+    if (team[1].getRole() === "manager") {
+      generateManager(team[i]);
+    }
   }
 };
